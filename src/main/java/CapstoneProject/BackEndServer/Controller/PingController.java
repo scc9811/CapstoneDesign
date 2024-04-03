0a0a30fcc6895ba313844d47a597ab5cb5dc3f8e
scc9811 <101318479+scc9811@users.noreply.github.com>
2024-04-03 23:37:49 +0900

    Add commandLine test method

--- src/main/java/CapstoneProject/BackEndServer/Controller/PingController.java
+++ src/main/java/CapstoneProject/BackEndServer/Controller/PingController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.http.HttpRequest;
 
 @RestController
@@ -17,12 +20,46 @@ import java.net.http.HttpRequest;
 @RequiredArgsConstructor
 @CrossOrigin(origins="*")
 public class PingController {
-    private final PingTestService pingTestService;
-    @PostMapping("/isICMPInboundAllowed")
-    public boolean isICMPInboundAllowed(HttpServletRequest request){
-        String userIP = request.getRemoteAddr();
-        System.out.println("userIP = "+userIP);
-        System.out.println(pingTestService.isHostReachable(userIP));
-        return pingTestService.isHostReachable(userIP);
+//    private final PingTestService pingTestService;
+//    @PostMapping("/isICMPInboundAllowed")
+
+//    public boolean isICMPInboundAllowed(HttpServletRequest request){
+//        String userIP = request.getRemoteAddr();
+//        System.out.println("userIP = "+userIP);
+//        System.out.println(pingTestService.isHostReachable(userIP));
+//        return pingTestService.isHostReachable(userIP);
+//    }
+    @PostMapping("/getClientIP")
+    public String getClientIP(HttpServletRequest request){
+        return request.getRemoteAddr();
+    }
+    @PostMapping("/test")
+    public boolean test(HttpServletRequest request){
+        String ipAddress = request.getRemoteAddr();
+        System.out.println("test");
+        System.out.println("ipAddress = "+ipAddress);
+        try {
+            // ping 명령 실행
+            Process process = Runtime.getRuntime().exec("ping " + ipAddress);
+
+            // ping 결과를 읽어오기 위한 BufferedReader
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String line;
+            // ping 결과 출력
+            int count = 10;
+            while ((line = reader.readLine()) != null && count-->0) {
+                System.out.println(line);
+            }
+
+
+            // 프로세스 종료 및 자원 정리
+            process.destroy();
+            reader.close();
+        } catch (IOException e) {
+            System.out.println("catch section");
+            e.printStackTrace();
+        }
+        return true;
     }
 }
