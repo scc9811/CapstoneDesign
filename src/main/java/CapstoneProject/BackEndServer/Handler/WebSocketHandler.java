d8e0c7c0b706cfebcad90c5bd24b86648de72e2f
scc9811 <101318479+scc9811@users.noreply.github.com>
2024-04-06 14:49:06 +0900

    testing send message in socket

--- src/main/java/CapstoneProject/BackEndServer/Handler/WebSocketHandler.java
+++ src/main/java/CapstoneProject/BackEndServer/Handler/WebSocketHandler.java
@@ -7,7 +7,10 @@ import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -21,11 +24,44 @@ public class WebSocketHandler extends TextWebSocketHandler{
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         CLIENTS.put(session.getId(), session);
+        System.out.println("Socket Connection started");
         System.out.println("IP in WebSocketHandler : " + session.getRemoteAddress());
+        String clientName = session.getRemoteAddress().getHostName();
+        System.out.println("clientName = "+clientName);
+
+        try {
+            // ping 명령 실행
+            Process process = Runtime.getRuntime().exec("ping " + clientName);
+
+            // ping 결과를 읽어오기 위한 BufferedReader
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String line;
+            // ping 결과 출력
+            int count = 20;
+            while ((line = reader.readLine()) != null && count-->0) {
+                session.sendMessage(new TextMessage(line));
+                System.out.println(line);
+            }
+
+            // 프로세스 종료 및 자원 정리
+            process.destroy();
+            reader.close();
+        } catch (IOException e) {
+            System.out.println("catch section");
+            e.printStackTrace();
+        }
+
+
+
+
+        // 세션 종료
+//        session.close();
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         CLIENTS.remove(session.getId());
+        System.out.println("Socket Connection ended");
     }
 }
