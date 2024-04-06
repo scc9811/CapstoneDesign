382b8e1c3610398365c5adf73c8e57d8d8875603
scc9811 <101318479+scc9811@users.noreply.github.com>
2024-04-06 15:20:01 +0900

    ping response average time test

--- src/main/java/CapstoneProject/BackEndServer/Handler/WebSocketHandler.java
+++ src/main/java/CapstoneProject/BackEndServer/Handler/WebSocketHandler.java
@@ -11,16 +11,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 // Compoent로 bean 등록하는 이유 ..?
 @Component
 public class WebSocketHandler extends TextWebSocketHandler{
     private static final Map<String, WebSocketSession> CLIENTS = new HashMap<>();
 
+    private static final long TIMEOUT = 1000;
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         CLIENTS.put(session.getId(), session);
@@ -36,12 +34,27 @@ public class WebSocketHandler extends TextWebSocketHandler{
             // ping 결과를 읽어오기 위한 BufferedReader
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
 
-            String line;
+
             // ping 결과 출력
-            int count = 20;
-            while ((line = reader.readLine()) != null && count-->0) {
-                session.sendMessage(new TextMessage(line));
+            String line;
+            int responseCount = 0;
+            double totalResponseTime = 0.0;
+            long startTime = System.currentTimeMillis();
+            while ((line = reader.readLine()) != null &&
+                    (System.currentTimeMillis() - startTime) < TIMEOUT
+                    && responseCount++ < 20) {
+                totalResponseTime += Double.parseDouble(line.split(" ")[6].split("=")[1]);
+                System.out.println("totalResponseTime = "+totalResponseTime);
+                double averageResponseTime = totalResponseTime / responseCount;
+                session.sendMessage(new TextMessage(String.valueOf(averageResponseTime)));
                 System.out.println(line);
+
+                startTime = System.currentTimeMillis();
+            }
+
+            // ping test denied
+            if(responseCount == 0){
+                System.out.println("ping test 불가. 방화벽 설정을 확인해주세요.");
             }
 
             // 프로세스 종료 및 자원 정리
@@ -55,8 +68,8 @@ public class WebSocketHandler extends TextWebSocketHandler{
 
 
 
-        // 세션 종료
-//        session.close();
+//         세션 종료
+        session.close();
     }
 
     @Override
